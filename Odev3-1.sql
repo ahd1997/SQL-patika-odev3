75484542a95961ac5ea37cf8680d7feb4ecc1301
ahd1997 <76910474+ahd1997@users.noreply.github.com>
2022-03-06 14:01:11 +0300

    Update Odev3-1.sql

--- Odev3-1.sql
+++ Odev3-1.sql
@@ -1,3 +1,3 @@
 --dvdrental örnek veri tabanı üzerinden country tablosunda bulunan country sütunundaki ülke isimlerinden 'A' karakteri ile başlayıp 'a' karakteri ile sonlananları sıralayınız.
 SELECT country FROM country
-WHERE country LIKE 'A%a'
+WHERE country LIKE 'A%a';
